c8368911c0c698d6c1961baa30bb441c4f37edda
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2022-06-01 17:37:21 -0300

    render api data

--- cornflix/src/pages/Home.js
+++ cornflix/src/pages/Home.js
@@ -2,9 +2,19 @@ import React from 'react'
 import useApi from '../services/Api'
 
 export default function Home() {
-  const filmsOrSeries = useApi('lost 2004')
-  console.log(filmsOrSeries);
+  const filmsOrSeries = useApi('xuxa')
   return (
-    <h1>Hello Word!</h1>
+    <main>
+      <h1>Cornflix</h1>
+      {filmsOrSeries === null ? <h1>loading...</h1> :
+        filmsOrSeries.map(({id, image, title, description}) => 
+          <div key={id}>
+            <h1>{title}</h1>
+            <img alt={title} src={image}/>
+            <h2>{description}</h2>
+          </div>
+        )
+      }
+    </main>
   )
 }
--- cornflix/src/services/Api.js
+++ cornflix/src/services/Api.js
@@ -1,16 +1,16 @@
 import React from "react";
 import axios from "axios";
 
-export default function useApi(index) {
-  const baseURL = `https://imdb-api.com/en/API/SearchTitle/k_wpt2r7c0/${index}`;
+export default function useApi(string) {
+  const baseURL = `https://imdb-api.com/en/API/SearchTitle/k_wpt2r7c0/${string}`;
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
     axios.get(baseURL)
-      .then((res) => {
-        return res.data;
+      .then(({ data }) => {
+        return data;
       })
-      .then(({results}) => {
+      .then(({ results }) => {
         setData(results)
       })
   }, [])
