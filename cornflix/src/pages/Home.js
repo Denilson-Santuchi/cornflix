2d1908ea7ee589e86fb975b61493accf73edff8c
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2022-06-02 16:08:43 -0300

    add loading spinner

--- cornflix/src/pages/Home.js
+++ cornflix/src/pages/Home.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import useApi from '../services/Api'
 
+import LoadingSpinner from '../components/LoadingSpinner';
+
 export default function Home() {
   const filmsOrSeries = useApi()
   console.log(filmsOrSeries);
   return (
     <main>
       <h1>Cornflix</h1>
-      {filmsOrSeries === null ? <h1>loading...</h1> :
-        filmsOrSeries.map(({id, image, title, crew}) => 
+      {filmsOrSeries === null ? <LoadingSpinner /> :
+        filmsOrSeries.map(({id, image, title, crew, year}) => 
           <div key={id}>
             <h1>{title}</h1>
             <img alt={title} src={image}/>
-            <h2>{crew}</h2>
+            <h2>{`${crew} - ${year}.`}</h2>
           </div>
         )
       }
